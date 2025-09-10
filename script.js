e1888661b1709efde1f6a43ec05b90db9e0cb441
Suireyha <Marvink2296@gmail.com>
2025-09-10 14:47:22 +1000

    Added event listeners and varaiables for the three button elements

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ const overrideFall = document.querySelector('#overrideFall');
 let warning = false;
 
 motionButton.addEventListener('click', ()=>{
+    console.log('Motion button clicked');
+});
 
-    
+overrideLift.addEventListener('click', ()=>{
+    console.log('Override lift clicked');
+});
+
+overrideFall.addEventListener('click', ()=>{
+    console.log('Override fall clicked');
 });
