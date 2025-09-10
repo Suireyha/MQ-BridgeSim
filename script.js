25f3aa65d1ca3cdc49d71102e3989f64d855a1d1
Suireyha <Marvink2296@gmail.com>
2025-09-10 14:40:40 +1000

    Wrote the bulk of the HTML for the project, with parents divs and set up the appropriate relationships

---
+++ script.js
@@ -0,0 +1,10 @@
+const motionButton = document.querySelector('#motionButton');
+const overrideLift = document.querySelector('#overrideLift');
+const overrideFall = document.querySelector('#overrideFall');
+
+let warning = false;
+
+motionButton.addEventListener('click', ()=>{
+
+    
+});
